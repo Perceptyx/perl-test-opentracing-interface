44c983283cdb44794d9139dc71194a7599d824a4
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-02-20 12:48:21 +0100

    fix issue with hardcoded test module 'Scope'

--- t/lib/Test/OpenTracing/Interface/Tester.pm
+++ t/lib/Test/OpenTracing/Interface/Tester.pm
@@ -1,5 +1,8 @@
 package Test::OpenTracing::Interface::Tester;
 
+use strict;
+use warnings;
+
 use Exporter 'import';
 
 our @EXPORT_OK = qw/run_tests_can_interface_ok/;
@@ -12,6 +15,8 @@ sub run_tests_can_interface_ok {
     my @interface_methods = @_;
     
     my $test_class = 'MyTest::' . $interface_name;
+    my $test_module_name = 'Test::OpenTracing::Interface::' . $interface_name;
+    my $test_method_name = join '::', $test_module_name, 'can_interface_ok';
     
     my @diag_messages = map {
         "MyTest::Empty->can('$_') failed"
@@ -19,18 +24,20 @@ sub run_tests_can_interface_ok {
     
     my $test_obj = bless {}, $test_class;
     
+    no strict qw/refs/;
+    
     test_out( "ok 1 - $test_class->can_interface( '$interface_name' )" );
-    Test::OpenTracing::Interface::Scope::can_interface_ok( $test_obj );
+    $test_method_name->( $test_obj );
     test_test( "Tests PASS when 'can_interface_ok' for object" );
     
     test_out( "ok 1 - $test_class->can_interface( '$interface_name' )" );
-    Test::OpenTracing::Interface::Scope::can_interface_ok( $test_class );
+    $test_method_name->( $test_class );
     test_test( "Tests PASS when 'can_interface_ok' for class" );
     
     test_out( "not ok 1 - MyTest::Empty->can_interface( '$interface_name' )");
     test_diag( sort @diag_messages );
     test_fail( +1 );
-    Test::OpenTracing::Interface::Scope::can_interface_ok( 'MyTest::Empty' );
+    $test_method_name->( 'MyTest::Empty' );
     test_test( "Tests FAIL when 'can_interface_ok' for bad class" );
     
     return
