713643d5b586ecbac5b1bcf463e7026fb794399e
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-02-20 09:34:22 +0100

    fix test `can_interface_ok` for `MyTest::Empty` class

--- lib/Test/OpenTracing/Interface/Scope.pm
+++ lib/Test/OpenTracing/Interface/Scope.pm
@@ -7,6 +7,12 @@ use Scalar::Util qw/blessed/;
 
 use Test::Builder;
 
+my $interface_name = 'Scope';
+my @interface_methods = qw{
+    close
+    get_span
+};
+
 my $Test = Test::Builder->new;
 
 sub can_interface_ok {
@@ -14,7 +20,18 @@ sub can_interface_ok {
     
     my $thing_name = blessed( $thing ) // "$thing";
     
-    return $Test->ok( 1, "${thing_name}->can_interface( 'Scope' )" )
+    my $test_name = "${thing_name}->can_interface( '$interface_name' )";
+    
+    my @failures;
+    foreach my $method ( sort @interface_methods ) {
+        unless ( $thing->can($method) ) {
+            $Test->diag("${thing_name}->can('$method') failed");
+            push @failures, $method;
+        }
+    }
+    
+    my $ok = scalar @failures ? 0 : 1;
+    return $Test->ok( $ok, $test_name );
     
 }
 
