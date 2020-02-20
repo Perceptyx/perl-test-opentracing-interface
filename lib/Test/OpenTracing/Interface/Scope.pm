f6c5a9bafc18d1e11f8b48c8a5b41cae86f3c226
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-02-20 17:27:01 +0100

    use proper class `..Interface::TestRunner`

--- lib/Test/OpenTracing/Interface/Scope.pm
+++ lib/Test/OpenTracing/Interface/Scope.pm
@@ -1,20 +1,19 @@
 package Test::OpenTracing::Interface::Scope;
 
-use Test::OpenTracing::TestRunner qw/run_tests_can_interface_ok/;
-
-my $interface_name = 'Scope';
-my @interface_methods = qw{
-    close
-    get_span
-};
+use Test::OpenTracing::TestRunner;
 
 sub can_interface_ok {
     my $thing = shift;
     
-    return run_tests_can_interface_ok(
-        $thing,
-        $interface_name => @interface_methods
-    )
+    my $Test = Test::OpenTracing::TestRunner->new(
+        interface_name => 'Scope',
+        interface_methods => [ 
+            'close',
+            'get_span',
+        ],
+    );
+    
+    return $Test->run_tests_can_interface_ok( $thing );
 }
 
 1;
--- lib/Test/OpenTracing/TestRunner.pm
+++ lib/Test/OpenTracing/TestRunner.pm
@@ -3,35 +3,53 @@ package Test::OpenTracing::TestRunner;
 use strict;
 use warnings;
 
-use Exporter 'import';
-our @EXPORT_OK = qw/run_tests_can_interface_ok/;
+use Object::Tiny qw/interface_name interface_methods/;
 
 use Scalar::Util qw/blessed/;
 
 use Test::Builder;
 
 sub run_tests_can_interface_ok{
+    my $self = shift;
     my $thing = shift;
     
-    my $interface_name = shift;
-    my @interface_methods = @_;
-     
-    my $thing_name = blessed( $thing ) // "$thing";
-    
-    my $test_name = "${thing_name}->can_interface( '$interface_name' )";
+    my $test_class_name = $self->test_class_name($thing);
     
     my $Test = Test::Builder->new;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
     
+    no strict qw/refs/;
     my @failures;
-    foreach my $method ( sort @interface_methods ) {
-        unless ( $thing->can($method) ) {
-            $Test->diag("${thing_name}->can('$method') failed");
-            push @failures, $method;
+    foreach my $test_method ( sort @{$self->interface_methods} ) {
+        unless ( $thing->can($test_method) ) {
+            $Test->diag("$test_class_name->can('$test_method') failed");
+            push @failures, $test_method;
         }
     }
     
+    my $test_name = $self->test_name( $thing );
     my $ok = scalar @failures ? 0 : 1;
     return $Test->ok( $ok, $test_name );
     
 }
+
+sub test_class_name {
+    my $self = shift;
+    my $thing = shift;
+    
+    return blessed( $thing ) // "$thing";
+}
+
+sub test_name {
+    my $self = shift;
+    my $thing = shift;
+    
+    my $test_class_name = $self->test_class_name($thing);
+    my $interface_name = $self->interface_name;
+    
+    return "${test_class_name}->can_interface( '$interface_name' )"
+}
+
+
+
+1;
