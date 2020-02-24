c9392fc1665daf1ea2700fe7aed3ea41d9121c82
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-02-24 17:02:07 +0100

    add `Test::OT::Interface::Span`
    and tests

---
+++ lib/Test/OpenTracing/Interface/Span.pm
@@ -0,0 +1,24 @@
+package Test::OpenTracing::Interface::Span;
+
+use Test::OpenTracing::Test::CanInterface;
+
+sub can_interface_ok {
+    my $thing = shift;
+    
+    my $Test = Test::OpenTracing::Test::CanInterface->new(
+        interface_name => 'Span',
+        interface_methods => [
+            'finish',
+            'get_baggage_item',
+            'get_context',
+            'log_data',
+            'overwrite_operation_name',
+            'set_baggage_item',
+            'set_tag',
+        ],
+    );
+    
+    return $Test->run_tests( $thing );
+}
+
+1;
