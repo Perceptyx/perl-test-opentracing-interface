f11e144f5c85e4a718b11d44bc5bed6b08d380ab
Theo van Hoesel <tvanhoesel@perceptyx.com>
2020-02-28 15:44:58 +0100

    use superclass for `Test::OpenTracing`
    attributes
    - interface_name
    - this

--- lib/Test/OpenTracing.pm
+++ lib/Test/OpenTracing.pm
@@ -1,23 +1,62 @@
 package Test::OpenTracing;
 
+use strict;
+use warnings;
+
 our $VERSION = '0.01_00';
+
+
+
+use Moo;
+
+
+
+use Scalar::Util qw/blessed/;
+
+
+
+has interface_name => (
+    is => 'ro',
+);
+
+has this => (
+    is => 'ro',
+);
+
+sub this_name {
+    my $self = shift;
+    
+    return blessed( $self->this ) // $self->this;
+}
+
+
 
 package Test::OpenTracing::CanInterface;
 
 use strict;
 use warnings;
 
-use Object::Tiny qw/interface_name interface_methods/;
 
-use Scalar::Util qw/blessed/;
+
+use Moo;
+extends 'Test::OpenTracing';
+
+
 
 use Test::Builder;
 
+
+
+has interface_methods => (
+    is => 'ro',
+);
+
+
+
 sub run_tests{
     my $self = shift;
-    my $this = shift;
     
-    my $test_class_name = $self->test_class_name($this);
+    my $test_name = $self->this_name;
     
     my $Test = Test::Builder->new;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
@@ -25,33 +64,35 @@ sub run_tests{
     no strict qw/refs/;
     my @failures;
     foreach my $test_method ( sort @{$self->interface_methods} ) {
-        unless ( $this->can($test_method) ) {
-            $Test->diag("$test_class_name->can('$test_method') failed");
+        unless ( $self->this->can($test_method) ) {
+            $Test->diag(
+                $self->this_name
+                .
+                "->can('"
+                .
+                $test_method
+                .
+                "') failed"
+            );
             push @failures, $test_method;
         }
     }
     
-    my $test_message = $self->test_message( $this );
+    my $test_message = $self->test_message( $self->this );
     my $ok = scalar @failures ? 0 : 1;
     return $Test->ok( $ok, $test_message );
     
 }
 
-sub test_class_name {
-    my $self = shift;
-    my $this = shift;
-    
-    return blessed( $this ) // "$this";
-}
 
 sub test_message {
     my $self = shift;
     my $this = shift;
     
-    my $test_class_name = $self->test_class_name($this);
+    my $this_name = $self->this_name();
     my $interface_name = $self->interface_name;
     
-    return "${test_class_name}->can_interface( '$interface_name' )"
+    return "$this_name->can_interface( '$interface_name' )"
 }
 
 
